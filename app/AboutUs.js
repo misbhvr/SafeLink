ef2b9c10332298a3d373cae523fbadf35ff7054a
Raello26 <141300296+Raello26@users.noreply.github.com>
2023-10-23 15:41:48 +1300

    adding menu pages
    Added the setting page and aboutus page

---
+++ app/AboutUs.js
@@ -0,0 +1,11 @@
+import React, {useEffect} from 'react';
+import {View, Text, TouchableOpacity, FlatList, ActivityIndicator, Linking, Image, SafeAreaView, ScrollView} from "react-native";
+import {Stack, useRouter} from 'expo-router';
+import {COLORS, icons, SIZES, images} from '../constants'
+import {ScreenHeaderBtn} from "../components";
+import styles from "./donate.style"
+import { useNavigation } from "expo-router";
+
+const AboutUsPage = () => {
+
+}
